f1a9d54ea0997c6386c59801180944bf2a7428f0
Controladoriagranja <controladoriagb05@gmail.com>
2026-09-21 14:47:08 -0300

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+PARQUET_BASE_DINAMICA = Path(
+    r"\\192.168.1.139\Controladoria\BI_Granja\Tratados\Indice Zootecnico\indice_zootecnico_base_dinamica_tratado.parquet"
+)

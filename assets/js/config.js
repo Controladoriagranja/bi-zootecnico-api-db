850da0c37741607947830937f5a964948636c70b
Controladoriagranja <controladoriagb05@gmail.com>
2026-09-21 14:45:04 -0300

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,18 @@
+const APP_CONFIG = {
+
+    API_URL: "http://127.0.0.1:8000",
+
+    endpoints: {
+
+        resumo:
+            "/api/zootecnico/resumo",
+
+        detalhes:
+            "/api/zootecnico/detalhes",
+
+        filtros:
+            "/api/zootecnico/filtros"
+
+    }
+
+};
